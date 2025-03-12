baa75f96313f805d11d021189e80234709520479
Lily Maclaren <3029139M@student.gla.ac.uk>
2025-03-12 14:05:43 +0000

    ch6 tests

--- rango/views.py
+++ rango/views.py
@@ -4,10 +4,12 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    most_viewed_pages = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
     context_dict['boldmessage'] =  'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = most_viewed_pages
     
     return render(request, 'rango/index.html', context=context_dict)
 
